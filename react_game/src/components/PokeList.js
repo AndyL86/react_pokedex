25d1da78f4f5a650de78217d8099c72d8d71c7d7
AndyL86 <alinehan89@outlook.com>
2023-09-23 14:04:58 +0000

    finish

--- react_game/src/components/PokeList.js
+++ react_game/src/components/PokeList.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import PokeCell from './PokeCell';
+import { pokeClasses } from '../pokeClasses';
 import './styles/PokeList.css';
 
-const PokeList = () => {
+const PokeList = ({handleOnClick}) => {
+    const cells = pokeClasses.map(pokeClass =>  {
+        return (
+            <PokeCell 
+              key={pokeClass.id}
+              pokeClass={pokeClass}
+              handleOnClick={handleOnClick}
+            />
+        );
+    });
+
     return (
         <section className='poke-list'>
-
+            {cells}
         </section>
     )
 }
